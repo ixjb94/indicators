99a0bbaac6c25665880436b555b20fd24222b05c
IX JB <ixjb94@gmail.com>
2023-11-24 13:24:08 +0000

    dc, also order

--- tests/indicators.test.ts
+++ tests/indicators.test.ts
@@ -58,6 +58,12 @@ class Helper {
 // ################################# Test
 describe('Indicators', () => {
 
+    it("Average True Range ::: atr 14", async () => {
+        const indicator = await ta.atr(high, low, close, 14)
+        const result = new Helper(indicator).lastNReverse(3).removeFloat(5).getResult()
+        expect(result).toEqual([0.00021, 0.00022, 0.00023])
+    })
+
     it("Bollinger Bands Lower ::: bbands 20, 2", async () => {
         const indicator = await ta.bbands(close, 20, 2)
         const result = new Helper(indicator[0]).lastNReverse(3).removeFloat(5).getResult()
@@ -76,19 +82,30 @@ describe('Indicators', () => {
         expect(result).toEqual([0.07646, 0.07644, 0.07643])
     })
 
-
-    it("Average True Range ::: atr 14", async () => {
-        const indicator = await ta.atr(high, low, close, 14)
-        const result = new Helper(indicator).lastNReverse(3).removeFloat(5).getResult()
-        expect(result).toEqual([0.00021, 0.00022, 0.00023])
-    })
-
     it("Commodity Channel Index ::: cci 20", async () => {
         const indicator = await ta.cci(high ,low, close, 20)
         const result = new Helper(indicator).lastNReverse(3).removeFloat(5).getResult()
         expect(result).toEqual([55.33649, 53.82779, 70.93023])
     })
     
+    it("Donchian Channels Upper ::: dc 20", async () => {
+        const indicator = await ta.dc(high, low, 20)
+        const result = new Helper(indicator[0]).lastNReverse(3).removeFloat(5).getResult()
+        expect(result).toEqual([0.07640, 0.07640, 0.07640])
+    })
+
+    it("Donchian Channels Middle ::: dc 20", async () => {
+        const indicator = await ta.dc(high, low, 20)
+        const result = new Helper(indicator[1]).lastNReverse(3).removeFloat(5).getResult()
+        expect(result).toEqual([0.0760, 0.0760, 0.0760])
+    })
+
+    it("Donchian Channels Lower ::: dc 20", async () => {
+        const indicator = await ta.dc(high, low, 20)
+        const result = new Helper(indicator[2]).lastNReverse(3).removeFloat(5).getResult()
+        expect(result).toEqual([0.07559, 0.07559, 0.07559])
+    })
+
     it("Exponential Moving Average ::: ema 10", async () => {
         const indicator = await ta.ema(close, 10)
         const result = new Helper(indicator).lastNReverse(3).removeFloat(5).getResult()
