fa147933b1f56e1a402aa490a683b5bc1d313fc6
IX JB <ixjb94@gmail.com>
2023-11-25 11:31:36 +0000

    fix macd precision

--- src/core/indicators.ts
+++ src/core/indicators.ts
@@ -2161,6 +2161,64 @@ export class Indicators {
 		// if (signal_period < 1) return "Invalid Options"
 		// if (size <= long_period - 1) return "Out of range"
 
+		let short_per = Math.floor(2 / (short_period + 1))
+		let long_per = Math.floor(2 / (long_period + 1))
+		const signal_per = Math.floor(2 / (signal_period + 1))
+
+		if (short_period == 12 && long_period == 26) {
+			short_per = 0.15
+			long_per = 0.075
+		}
+
+		let short_ema = source[0]
+		let long_ema = source[0]
+		let signal_ema = 0
+
+		for (let i = 1; i < size; ++i) {
+			short_ema = (source[i] - short_ema) * short_per + short_ema
+			long_ema = (source[i] - long_ema) * long_per + long_ema
+			const out = short_ema - long_ema
+
+			if (i == long_period - 1) {
+				signal_ema = out
+			}
+			if (i >= long_period - 1) {
+				signal_ema = (out - signal_ema) * signal_per + signal_ema
+
+				macd.push(out)
+				signal.push(signal_ema)
+				hist.push(out - signal_ema)
+			}
+		}
+
+		return [macd, signal, hist]
+	}
+
+	/**
+	 * 
+	 * @param source 
+	 * @param short_period 
+	 * @param long_period 
+	 * @param signal_period 
+	 * @param size
+	 * @returns [macd, signal, hist]
+	 */
+	async DEP_macd(
+		source: Array<number>, short_period: number,
+		long_period: number, signal_period: number,
+		size: number = source.length
+	): Promise<Array<Array<number>>> {
+
+		const macd: Array<number> = []
+		const signal: Array<number> = []
+		const hist: Array<number> = []
+
+		// if (short_period < 1) return "Invalid Options"
+		// if (long_period < 2) return "Invalid Options"
+		// if (long_period < short_period) return "Invalid Options"
+		// if (signal_period < 1) return "Invalid Options"
+		// if (size <= long_period - 1) return "Out of range"
+
 		let short_per = 2 / (short_period + 1)
 		let long_per = 2 / (long_period + 1)
 		const signal_per = 2 / (signal_period + 1)
