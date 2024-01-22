5ad4abc7483f78ca6e74ffa7f59bcd47ec07357e
ixjb94 <ixjb94@gmail.com>
2024-01-22 08:25:31 +0000

    crossOverNumber, crossUnderNumber

--- src/core/indicatorsNormalized.ts
+++ src/core/indicatorsNormalized.ts
@@ -946,6 +946,59 @@ export class IndicatorsNormalized {
 	}
 
 	/**
+	 * @param seriesA 
+	 * @param number 
+	 * @returns 
+	 */
+	crossOverNumber(seriesA: number[], number: number) {
+    
+		const output = []
+	
+		for (let index = 0; index < seriesA.length; index++) {
+			const current = seriesA[index]
+			const pre     = seriesA[index - 1]
+			
+			if (
+				current > number &&
+				pre <= number
+			) {
+				output.push(true)
+			} else {
+				output.push(false)
+			}
+		}
+	
+		return output
+	}
+	
+	/**
+	 * 
+	 * @param seriesA 
+	 * @param number 
+	 * @returns 
+	 */
+	crossUnderNumber(seriesA: number[], number: number) {
+		
+		const output = []
+	
+		for (let index = 0; index < seriesA.length; index++) {
+			const current = seriesA[index]
+			const pre     = seriesA[index - 1]
+			
+			if (
+				current < number &&
+				pre >= number
+			) {
+				output.push(true)
+			} else {
+				output.push(false)
+			}
+		}
+	
+		return output
+	}
+
+	/**
 	 * @Updated
 	 * @param high 
 	 * @param low 
