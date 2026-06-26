039500e54759db57b71a4551f3601c15ecaef983
Alejo Castillo <alejoworkstuff@gmail.com>
2026-06-26 11:40:32 -0300

    fix: ensure integer index in DPO for odd periods (#24)

--- src/core/extract/dpo.ts
+++ src/core/extract/dpo.ts
@@ -1,5 +1,4 @@
 
-
 /**
  * 
  * @param source 
@@ -12,7 +11,7 @@ export async function dpo(
     size: number = source.length
 ) {
 
-    const back = (period >> 1) + 1
+    const back = Math.floor(period / 2) + 1
 
     const output: Array<number> = []
 
